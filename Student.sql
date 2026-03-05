3e4a02f03f4f4d9f20fc2ee559b6acee7159de78
parikhdev <itsyashparikh@gmail.com>
2026-03-05 06:44:20 +0530

    Added other search features , subqueries

--- Student.sql
+++ Student.sql
@@ -26,4 +26,48 @@ VALUES
 SELECT * FROM students;
 SELECT * FROM test_attempts;
 
+INSERT INTO test_attempts(attempt_id,student_id,subject,score)
+VALUES
+(105,1,'JEE Math Mock',92),
+(106,3,'JEE Math Mock',94),
+(107,1,'JEE Math Mock',97),
+(108,1,'JEE Math Mock',89);
 
+SELECT attempt_id,subject,score 
+FROM test_attempts 
+WHERE subject = 'JEE Math Mock' 
+ORDER BY score DESC;
+-- LIMIT = 5;
+
+INSERT INTO test_attempts (attempt_id,student_id,subject,score)
+VALUES 
+(109, 1, 'JEE Math Mock', 94),
+(110, 3, 'JEE Math Mock', 98),
+(111, 1, 'JEE Math Mock', 91),
+(112, 1, 'JEE Math Mock', 97);
+
+SELECT attempt_id, subject, score
+FROM (
+    SELECT attempt_id, subject, score 
+    FROM test_attempts
+    ORDER BY score DESC
+    Limit 7
+) AS top_scores
+WHERE subject = 'JEE Math Mock'
+ORDER BY score ASC;
+
+SELECT *
+FROM students
+INNER JOIN test_attempts ON test_attempts.student_id = students.student_id;
+
+SELECT students.name, test_attempts.subject, AVG(test_attempts.score) AS average_score
+FROM test_attempts
+INNER JOIN students ON students.student_id = test_attempts.student_id
+GROUP BY students.name, test_attempts.subject
+ORDER BY average_score DESC;
+
+SELECT students.name, COUNT(test_attempts.attempt_id) AS total_tests_taken
+FROM students
+LEFT JOIN test_attempts ON students.student_id = test_attempts.student_id
+GROUP BY students.name
+ORDER BY total_tests_taken DESC;
